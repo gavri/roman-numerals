8b2960fc24b609dd0d086b00ba87e42752f27cc5
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-08-22 13:20:12 +0700

    Extend repeating/addition rule to X, C, M

--- src/main/scala/Roman.scala
+++ src/main/scala/Roman.scala
@@ -1,7 +1,15 @@
 package com.example.gavri.romannumerals
 
 case class Roman(s: String) {
+
+  val repeatingAdditionMapping = Map(
+    'I' -> 1,
+    'X' -> 10,
+    'C' -> 100,
+    'M' -> 1000
+  )
+
   def value: Int = {
-    s.length
+    s.map((c: Char) => repeatingAdditionMapping(c)).sum
   }
 }
--- src/test/scala/RomanSpec.scala
+++ src/test/scala/RomanSpec.scala
@@ -4,10 +4,19 @@ import org.scalatest._
 
 class RomanSpec extends FunSpec with Matchers {
   describe("Roman") {
-    it("should understand the repeating rule") {
+    it("should understand the repeating/additon rule") {
       Roman("I").value should equal (new Integer(1))
       Roman("II").value should equal (new Integer(2))
       Roman("III").value should equal (new Integer(3))
+      Roman("X").value should equal (new Integer(10))
+      Roman("XX").value should equal (new Integer(20))
+      Roman("XXX").value should equal (new Integer(30))
+      Roman("C").value should equal (new Integer(100))
+      Roman("CC").value should equal (new Integer(200))
+      Roman("CCC").value should equal (new Integer(300))
+      Roman("M").value should equal (new Integer(1000))
+      Roman("MM").value should equal (new Integer(2000))
+      Roman("MMM").value should equal (new Integer(3000))
     }
   }
 }

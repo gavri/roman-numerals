daf9f9004bebb6f1d7d5326faae09805700a2f96
Gavri Fernandez <gavri.fernandez@gmail.com>
2017-08-22 13:47:18 +0700

    Understand decreasing rule (works as non-decreasing for now)

--- src/test/scala/RomanSpec.scala
+++ src/test/scala/RomanSpec.scala
@@ -30,6 +30,24 @@ class RomanSpec extends FunSpec with Matchers {
       assertRomanValue("L", 50)
       assertRomanValue("D", 500)
     }
+
+    it("should understand decreasing rule") {
+      assertRomanValue("VI", 6)
+      assertRomanValue("VII", 7)
+      assertRomanValue("VIII", 8)
+      assertRomanValue("XI", 11)
+      assertRomanValue("XII", 12)
+      assertRomanValue("XVI", 16)
+      assertRomanValue("LI", 51)
+      assertRomanValue("LII", 52)
+      assertRomanValue("LV", 55)
+      assertRomanValue("LVI", 56)
+      assertRomanValue("LVII", 57)
+      assertRomanValue("LIII", 53)
+      assertRomanValue("DI", 501)
+      assertRomanValue("DII", 502)
+      assertRomanValue("DIII", 503)
+    }
   }
 }
 
